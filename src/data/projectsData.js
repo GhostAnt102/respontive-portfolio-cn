ef564db023b8fe4415185cbcebb9177fbf81753a
Kien Ma <101681038+KienMa111@users.noreply.github.com>
2022-11-26 14:54:27 +0700

    uoload

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -12,29 +12,29 @@ export const projectsData = [
     
     {
         id: 2,
-        projectName: 'MARRIOTT BONOY CLONE',
-        projectDesc: 'It is a web application to book hotels.',
-        tags: ['HTML', 'CSS', 'JavaScript'],
-        code: 'https://github.com/webdeveshverma/Marriott_Bonvoy_clone.github.io',
-        demo: 'https://personal-iota-flame.vercel.app/',
+        projectName: 'Website to learn Japanese',
+        projectDesc: 'Website allows users to create their own goals or get goals from the template to learn.',
+        tags: ['Jira', 'Google sheet'],
+        // code: 'https://github.com/webdeveshverma/Marriott_Bonvoy_clone.github.io',
+        demo: 'https://smileeye.edu.vn/goals',
         image: 'https://i.ibb.co/HrWfTyr/Marriott.png'
     },
     {
         id: 3,
-        projectName: 'Agoda Clone',
-        projectDesc: 'Agoda is one of the world fastest growing online travel booking platforms. From its beginnings as an e-commerce start-up based in Singapore in 2005.',
-        tags: ['RACT','REDUX','MONGODB','JSX','EXPRESS','MONGOOSE'],
-        code: 'https://github.com/shailendra7518/Agoda-clone',
-        demo: 'https://agoda-clone.vercel.app/',
+        projectName: '390 most beautiful website templates',
+        projectDesc: 'The project launched a series of beautiful and unique website templates.',
+        tags: ['Jira', 'Google sheet'],
+        // code: 'https://github.com/shailendra7518/Agoda-clone',
+        // demo: 'https://agoda-clone.vercel.app/',
         image: 'https://i.ibb.co/5TJzyWQ/AGODA-CLONE.jpg'
     },
     {
         id: 4,
-        projectName: 'Weather App',
-        projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-        tags: ['Openweather API', 'JavaScript', 'HTML', 'CSS'],
-        code: 'https://github.com/webdeveshverma/Check-Weather-Appliction-',
-        demo: 'https://webdeveshverma.github.io/Check-Weather-Appliction-/',
+        projectName: 'Taxi booking application (Android)',
+        projectDesc: 'This application allows users to quickly communicate with drivers who use the app.',
+        tags: ['Jira', 'Google sheet'],
+        // code: 'https://github.com/webdeveshverma/Check-Weather-Appliction-',
+        demo: 'https://play.google.com/store/apps/details?id=com.educomapps.cabbooking&hl=vi&gl=UG&pli=1',
         image: 'https://i.ibb.co/C9hZWVN/Weather-app.png'
     },
    
